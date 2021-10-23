a83ba5906c31e1b778e07e8e8371520779d9fbe6
sikkwun <59135611+sikkwun@users.noreply.github.com>
2021-10-23 16:25:42 +0900

    microservice configuration setting
    dev configuration setting

---
+++ microservice/discoverservice/src/main/java/springcloud/discoverservice/DiscoverserviceApplication.java
@@ -0,0 +1,15 @@
+package springcloud.discoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverserviceApplication.class, args);
+	}
+
+}
---
+++ microservice/discoverservice/src/test/java/springcloud/discoverservice/DiscoverserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloud.discoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
